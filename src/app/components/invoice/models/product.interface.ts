bec9a1d76938fa8bcd61dda5016321ef954206ae
leonardo15043 <lahernandez15043@gmail.com>
2022-09-04 11:10:50 -0500

    Template action invoice

---
+++ src/app/components/invoice/models/product.interface.ts
@@ -0,0 +1,6 @@
+export interface Product{
+    description:string;
+    unit_value:number;
+    created_at?:Date;
+    updated_at?:Date;
+}
--- src/app/components/invoice/pages/invoice-list/invoice-list.component.ts
+++ src/app/components/invoice/pages/invoice-list/invoice-list.component.ts
@@ -2,7 +2,7 @@ import {Component} from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { Invoice } from '../../models/invoice.interface';
 
-const invoice: Invoice[] = [
+const invoices: Invoice[] = [
   { invoice_number: 1, created_at: new Date(), sender_name:'Leonardo', receiver_name:'arturo', amount: 2000, iva: 19, total: 200 },
   { invoice_number: 1, created_at: new Date(), sender_name:'Leonardo', receiver_name:'arturo', amount: 2000, iva: 19, total: 200 },
   { invoice_number: 1, created_at: new Date(), sender_name:'Leonardo', receiver_name:'arturo', amount: 2000, iva: 19, total: 200 },
@@ -18,10 +18,11 @@ const invoice: Invoice[] = [
   styleUrls: ['./invoice-list.component.scss']
 })
 export class InvoiceListComponent {
-  displayedColumns: string[] = ['invoice_number', 'created_at', 'sender_name', 'receiver_name', 'amount', 'iva', 'total', 'actions'];
-  public dataSource = new MatTableDataSource<Invoice>([]);
+
+  public displayedColumns: string[] = ['invoice_number', 'created_at', 'sender_name', 'receiver_name', 'amount', 'iva', 'total', 'actions'];
+  public dataInvoice = new MatTableDataSource<Invoice>([]);
   
   constructor(){
-    this.dataSource.data = invoice;
+    this.dataInvoice.data = invoices;
   }
 }
