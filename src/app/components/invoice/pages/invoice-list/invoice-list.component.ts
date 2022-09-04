2b7035aca411189f8032d300a4dc490d98002c4d
leonardo15043 <lahernandez15043@gmail.com>
2022-09-04 12:21:28 -0500

    the service was implemented to bring the list of invoices

--- src/app/components/invoice/pages/invoice-list/invoice-list.component.ts
+++ src/app/components/invoice/pages/invoice-list/invoice-list.component.ts
@@ -1,16 +1,8 @@
 import {Component} from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { Invoice } from '../../models/invoice.interface';
+import { InvoiceService } from '../../services/invoice.service';
 
-const invoices: Invoice[] = [
-  { invoice_number: 1, created_at: new Date(), sender_name:'Leonardo', receiver_name:'arturo', amount: 2000, iva: 19, total: 200 },
-  { invoice_number: 1, created_at: new Date(), sender_name:'Leonardo', receiver_name:'arturo', amount: 2000, iva: 19, total: 200 },
-  { invoice_number: 1, created_at: new Date(), sender_name:'Leonardo', receiver_name:'arturo', amount: 2000, iva: 19, total: 200 },
-  { invoice_number: 1, created_at: new Date(), sender_name:'Leonardo', receiver_name:'arturo', amount: 2000, iva: 19, total: 200 },
-  { invoice_number: 1, created_at: new Date(), sender_name:'Leonardo', receiver_name:'arturo', amount: 2000, iva: 19, total: 200 },
-  { invoice_number: 1, created_at: new Date(), sender_name:'Leonardo', receiver_name:'arturo', amount: 2000, iva: 19, total: 200 },
-  { invoice_number: 1, created_at: new Date(), sender_name:'Leonardo', receiver_name:'arturo', amount: 2000, iva: 19, total: 200 }
-];
 
 @Component({
   selector: 'app-invoice-list',
@@ -22,7 +14,16 @@ export class InvoiceListComponent {
   public displayedColumns: string[] = ['invoice_number', 'created_at', 'sender_name', 'receiver_name', 'amount', 'iva', 'total', 'actions'];
   public dataInvoice = new MatTableDataSource<Invoice>([]);
   
-  constructor(){
-    this.dataInvoice.data = invoices;
+  constructor(
+    private _invoiceService:InvoiceService
+  ){
+    this.getInvoices();
   }
+
+  public getInvoices(){
+    this._invoiceService.getInvoices().subscribe(( invoices:Invoice[] ) =>{
+      this.dataInvoice.data = invoices;
+    })
+  }
+
 }
---
+++ src/app/components/invoice/services/invoice.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map } from 'rxjs';
+import { environment } from '../../../../environments/environment';
+import { Invoice } from '../models/invoice.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InvoiceService {
+
+  private url = environment.service.url;
+  private headers: HttpHeaders;
+
+  constructor(
+    private http: HttpClient
+  ) {
+    this.headers = new HttpHeaders();
+    this.headers = this.headers.append('Authorization', 'Bearer ' + localStorage.getItem('token'));
+  }
+
+  getInvoices(){
+    return this.http.get<Invoice[]>(`${ this.url }invoice/all`, { headers: this.headers }).pipe( map( data => data ));
+  }
+
+
+}
